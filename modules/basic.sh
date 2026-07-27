dabae9057967e2e4c9c29b8f81fc7444273e756a
Adam <leffleradam@gmail.com>
2026-07-27 22:32:36 +0200

    Update basic.sh
    Added configuration of Fastfetch, as well as a systemd user service for automatic mounting of Rclone.

--- modules/basic.sh
+++ modules/basic.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # MODULE: basic.sh
-# DESCRIPTION: Installs the very basic utilities of the system. 
+# DESCRIPTION: Installs the very basic utilities of the system, configures Fastfetch and creates a systemd user service for Rclone mounting
 
 PACKAGES=(
     # System
@@ -28,3 +28,66 @@ PACKAGES=(
 
 
 sudo dnf install -y "${PACKAGES[@]}"
+
+
+
+# Rsync user service for automatic mounting 
+
+TARGET_USER="${SUDO_USER:-$USER}"
+USER_HOME=$(eval echo "~$TARGET_USER")
+TARGET_UID=$(id -u "$TARGET_USER")
+
+mkdir -p "$USER_HOME/Cloud"
+mkdir -p "$USER_HOME/.config/systemd/user"
+
+cat <<EOF > "$USER_HOME/.config/systemd/user/rclone-gdrive.service"
+[Unit]
+Description=Rclone mount for Google Drive
+AssertPathIsDirectory=%h/Cloud
+After=network-online.target
+
+[Service]
+Type=simple
+ExecStart=/usr/bin/rclone mount --vfs-cache-mode full google-drive:/ %h/Cloud
+ExecStop=/usr/bin/fusermount3 -uz %h/Cloud
+Restart=on-failure
+RestartSec=10
+
+[Install]
+WantedBy=default.target
+EOF
+
+chown -R "$TARGET_USER:$TARGET_USER" "$USER_HOME/Cloud" "$USER_HOME/.config/systemd"
+
+sudo -u "$TARGET_USER" XDG_RUNTIME_DIR="/run/user/$TARGET_UID" systemctl --user daemon-reload
+sudo -u "$TARGET_USER" XDG_RUNTIME_DIR="/run/user/$TARGET_UID" systemctl --user enable rclone-gdrive.service
+
+
+
+# Configuration of Fastfetch
+
+fastfetch --gen-config
+cat << 'EOF' > "$USER_HOME/.config/fastfetch/config.jsonc"
+{
+  "$schema": "https://github.com/fastfetch-cli/fastfetch/raw/dev/doc/json_schema.json",
+  "modules": [
+    "title",
+    "separator",
+    "os",
+    "kernel",
+    "uptime",
+    "packages",
+    "de",
+    "terminal",
+    "cpu",
+    "gpu",
+    "memory",
+    "disk",
+    "localip",
+  ]
+}
+EOF
+
+chown -R "$TARGET_USER:$TARGET_USER" "$USER_HOME/.config/fastfetch"
+
+echo "fastfetch" >> .bashrc
